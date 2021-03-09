0c4db3d1c70261fd2554e64de8857988e57f6005
Jinhun-Noh <76435029+Jinhun-Noh@users.noreply.github.com>
2021-03-09 14:43:56 +0900

    Player 스크립트 작성
    캐릭터 이동, 상 하 좌 우 애니메이션 각각 지정(임시 캐릭터 스프라이트 사용)

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rigidbody;
+    SpriteRenderer spriteRenderer;
+
+    Vector2 moveVector;
+
+    public float moveSpeed = 5f;
+
+    float
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        moveVector = new Vector2(h, v);
+
+        rigidbody.velocity = moveVector * moveSpeed;
+
+        if (animator.GetInteger("hAxisRaw") != h)
+        {
+            animator.SetBool("isChange", true);
+            animator.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (animator.GetInteger("vAxisRaw") != v)
+        {
+            animator.SetBool("isChange", true);
+            animator.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            animator.SetBool("isChange", false);
+        }
+    }
+
+    void CheckObject()
+    {
+
+    }
+}
