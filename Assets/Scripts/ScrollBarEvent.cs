13dbe723126d7ab13ccecb9a9abf58f44766f9fb
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-03-09 23:02:37 +0900

    shop UI , event
    1. Shop UI 출력
    -> 기본 상점 구성 돈,인벤,상점, 스크롤, 나가는 버튼, 등
    
    2-1. 스크롤뷰 사용 시 클릭으로도 드래그 되는 현상 비활성화
    2-2. 드래그 위,아래 버튼 누르면 드래그
    2-3. 우측 상단 x 버튼 누르면 상점 비활성화

---
+++ Assets/Scripts/ScrollBarEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScrollBarEvent : MonoBehaviour
+{
+    [SerializeField]
+    private Scrollbar scrollbar;
+    [SerializeField]
+    private Transform contents;
+    public void ScrollLine(bool dir)
+    {
+        int childCount = contents.childCount;
+        if (4 > childCount)
+            return;
+
+        if (dir)
+        {
+            scrollbar.value -= (1.0f / (childCount - 4.0f));
+        }
+        else
+        {
+            scrollbar.value += (1.0f / (childCount - 4.0f));
+        }
+    }
+}
