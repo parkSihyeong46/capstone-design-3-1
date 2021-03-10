c26749e3ada4deabf1c6f3c85931a687e038a16b
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-03-11 01:17:26 +0900

    inventory ui, evnent, add ui test button
    1. 인벤토리 UI 출력
    
    2. 인벤토리, 상점 기능
    ( debug.log(" ") )로 추상적으로 설계
    
    3. 인벤토리 작동방식 변경
    프리팹 -> enable, disabled로 변경,
    
    4. 인벤, 상점을 원할 때 볼 수 있도록 ui 출력 테스트 버튼 생성

---
+++ Assets/Scripts/InventoryInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryInit : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Debug.Log("캐릭터 인벤 정보를 통해 아이템 배열을 받아옴");
+        Debug.Log("받아온 아이템 배열을 통해 아이템 없는 자리의 아이템 이미지 disabled");
+        Debug.Log("아이템 있는 자리는 enabled, 해당 아이템으로 이미지 설정\n");
+
+        Debug.Log("캐릭터 장비 정보를 받아옴");
+        Debug.Log("장비 장착 중이면 해당 부분 이미지 변경, 장착 중이지 않으면, 지정해둔 기본 이미지 변경");
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ShopUIInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUIInit : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Debug.Log("캐릭터 인벤 정보를 통해 아이템 배열을 받아옴");
+        Debug.Log("받아온 아이템 배열을 통해 아이템 없는 자리의 아이템 이미지 disabled");
+        Debug.Log("아이템 있는 자리는 enabled, 해당 아이템으로 이미지 설정\n");
+
+        Debug.Log("상인 정보 를 통해 물품 프리팹 생성");
+        Debug.Log("contents에 자식으로 productButton 프리팹 생성 후 이미지, 상품이름, 가격 설정");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("contents 자식 요소 모두 삭제");
+    }
+}
