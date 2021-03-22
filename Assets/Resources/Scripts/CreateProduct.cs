dfc743f9552f90ae89dc2d98f917d489986ab7b9
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-03-22 19:42:42 +0900

    상점 데이터 로드
    상점 데이터를 읽고, 판매 아이템 세팅 후 출력

---
+++ Assets/Resources/Scripts/CreateProduct.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateProduct : MonoBehaviour
+{
+    [SerializeField]
+    private Transform productPrefab;
+
+    void Start()
+    {
+        List<Shop.CellItem> cellItems = transform.parent.parent.GetComponent<Shop>().GetCellItemList();
+
+        for(int i = 0; i < cellItems.Count; i++)
+        {
+            if (cellItems[i] == null)
+                continue;
+
+            CreateProductButton(cellItems[i]);
+        }
+    }
+
+    void CreateProductButton(Shop.CellItem cellItem)
+    {
+        Transform product = Instantiate(productPrefab);
+        product.SetParent(transform);
+
+        product.GetChild(0).GetComponent<Text>().text = cellItem.item.ItemName;
+        product.GetChild(2).GetComponent<Image>().sprite = cellItem.item.ItemImage;
+    }
+}
---
+++ Assets/Resources/Scripts/Shop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    private const string traderDataPath = "Data/traderData";
+    private TextAsset textAsset;
+    private List<CellItem> cellItemList = new List<CellItem>();
+    public class CellItem
+    {
+        public Item item;
+        public int count;
+
+        public CellItem(Item item, int count)
+        {
+            this.item = item;
+            this.count = count;
+        }
+    }
+
+    void Start()
+    {
+        textAsset = Resources.Load<TextAsset>(traderDataPath);
+        string[] line = textAsset.text.Substring(0, textAsset.text.Length - 1).Split('\n');
+        for (int i = 1; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+
+            try
+            {
+                cellItemList.Add(new CellItem(ItemManager.Instance.GetItem(Int32.Parse(row[0])).DeepCopy(), Int32.Parse(row[2])));
+            }
+            catch
+            {
+                cellItemList.Add(null);
+            }
+        }
+    }
+
+    public List<CellItem> GetCellItemList()
+    {
+        return cellItemList;
+    }
+}
