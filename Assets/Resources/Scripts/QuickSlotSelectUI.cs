7d2cd19ca432470a9a8789d1297cb72d64739ca6
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-04-06 14:18:45 +0900

    퀵슬롯 스크롤 구현
    퀵슬롯의 선택 스크롤 기능 구현
    - 마우스 휠 움직임에 따라 선택UI 위치가 이동

---
+++ Assets/Resources/Scripts/QuickSlotSelectUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSlotSelectUI : MonoBehaviour
+{
+    Vector2 wheelInput;
+    private float speed = 10.0f;
+
+    private const int FIRST_SELECT_UI_X = -302;
+    private const int LAST_SELECT_UI_X = 303;
+
+    [SerializeField]
+    private Transform quickSlotSelectUI;
+
+    private void Update()
+    {
+        wheelInput = Input.mouseScrollDelta;
+
+        if (wheelInput.y > 0) // 휠 올렸을 때
+        {
+            quickSlotSelectUI.Translate(Vector3.left * 55);
+
+            if (quickSlotSelectUI.localPosition.x < FIRST_SELECT_UI_X)
+            {
+                quickSlotSelectUI.localPosition = new Vector3(LAST_SELECT_UI_X, quickSlotSelectUI.localPosition.y, 0);
+            }
+        }
+        else if (wheelInput.y < 0) // 휠 내렸을 때
+        {
+            quickSlotSelectUI.Translate(Vector3.right * 55);
+
+            if (quickSlotSelectUI.localPosition.x > LAST_SELECT_UI_X)
+            {
+                quickSlotSelectUI.localPosition = new Vector3(FIRST_SELECT_UI_X, quickSlotSelectUI.localPosition.y, 0);
+            }
+        }
+    }
+}
