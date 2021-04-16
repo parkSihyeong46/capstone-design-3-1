59b47e736b1e5e830947417c6cdeb8c61b58124f
rsh6929 <80191108+rsh6929@users.noreply.github.com>
2021-04-16 18:35:57 +0900

    로딩

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnclickOnlineButton()
+    {
+        Debug.Log("click Online");
+        LoadingsceneController.LoadScene("123");
+
+    }
+    public void OnclickQuitButton()
+        {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit() // 어플리케이션 종료
+#endif
+    }
+}
