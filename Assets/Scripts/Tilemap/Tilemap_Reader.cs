1270d4cb7d3a4fb4a65a7cc23b21f6992bf10b9f
Jinhun-Noh <76435029+Jinhun-Noh@users.noreply.github.com>
2021-04-16 21:41:51 +0900

    타일맵리더, 픽셀 깨짐 수정
    기존에는 룰타일로 그렸으나 이제 일반 타일로 그리도록 바꿈. 경작 가능한 땅 추가하려면 ScriptableObject 폴더에 있는 Plowable에 타일 추가하면 됨

--- Assets/Scripts/Tilemap/Tilemap_Reader.cs
+++ Assets/Scripts/Tilemap/Tilemap_Reader.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-//땅으로 사용할 타일을 지금은 룰타일로 써서 데이터 비교하고있는데 이렇게 하는것보다
-//땅으로 사용할 스프라이트 리스트 같은걸 만들어서 사용하는게 맵 그릴 때 더 효과적일듯
-//룰타일로 적용한 타일은 여러개 묶음이 하나로 취급되기때문에 코드상으로 비교하기에는 더 쉽지만 맵 그릴 때 불편함
-
-
 public class Tilemap_Reader : MonoBehaviour
 {
     [SerializeField] Tilemap tilemap;
     [SerializeField] List<TileData> tileDatas;      //하이라키창에서 여기에 타일데이터 넣어줌(Plowable, NotPlowable)
-    Dictionary<TileBase, TileData> tileDictionary;  //경작지 타일 리스트
+    Dictionary<TileBase, TileData> tileDictionary;  //경작지 타일 딕셔너리
 
     public LayerMask layerMask_Object;  //오브젝트 판별 레이어
     public LayerMask layerMask_Crop;    //작물 판별 레이어
@@ -26,7 +21,7 @@ public class Tilemap_Reader : MonoBehaviour
         tileDictionary = new Dictionary<TileBase, TileData>();
         foreach (TileData tileData in tileDatas)
         {
-            foreach (TileBase tile in tileData.tiles)   //그 타일데이터의 타일
+            foreach (TileBase tile in tileData.tiles)   //여기서 tile은 Plowable 파일에 넣은 타일베이스
             {
                 tileDictionary.Add(tile, tileData);     //딕셔너리에 타일과 그 타일이 담고있는 정보(Plowable인지 NotPlowable인지)를 추가
             }
@@ -34,7 +29,6 @@ public class Tilemap_Reader : MonoBehaviour
     }
 
     //Vector3형을 Vector3Int형으로 변환하는 메소드(타일맵 마커 등에 사용)
-    //오브젝트 판별은 따로 분리하는 것도 나쁘지 않을 것으로 생각됨.(상호작용 하게될 오브젝트를 받아와야 하기 때문에)
     public Vector3Int MousePosToGridPos(Vector3 mousePos)
     {
         worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
@@ -45,10 +39,6 @@ public class Tilemap_Reader : MonoBehaviour
         //오브젝트 유무 판별
         if (hit_Object) { isObjectEmpty = false; }
         else { isObjectEmpty = true; }
-
-        //작물 유무 판별
-        //if (hit_Crop) { isCropEmpty = false; }
-        //else { isCropEmpty = true; }
 
         Vector3Int gridPosition = tilemap.WorldToCell(worldPosition);    //마우스좌표를 Vector3Int형으로 변환 하기위해 WorldToCell(마우스 좌표) 사용
         
@@ -73,22 +63,22 @@ public class Tilemap_Reader : MonoBehaviour
     //경작 여부 판별 메소드
     public bool GetMousePosTileData(Vector3Int gridPos)
     {
-        TileBase tileBase = tilemap.GetTile(gridPos);   //마우스 좌표가 있는 곳의 타일
-        TileData tileData;                              //그 타일의 데이터(참 거짓 여부)
+        TileBase mouseTileBase = tilemap.GetTile(gridPos);   //마우스 좌표가 있는 곳의 타일
+        TileData mouseTileData;                              //그 타일의 데이터(참 거짓 여부)
         bool isPlowable = false;
 
-        if (tileBase != null)
+        if (mouseTileBase != null)  //마우스 위치에 타일이 있을 때
         {
-            tileData = tileDictionary[tileBase];
+            mouseTileData = tileDictionary[mouseTileBase];  //타일데이터는 mouseTileBase를 키값으로 사용해 딕셔너리에 저장되어있는 데이터 대입
         }
         else
         {
-            tileData = tileDatas[0];
+            mouseTileData = tileDatas[0];
         }
 
-        if (tileData != null)
+        if (mouseTileData != null)
         {
-            if (tileData == tileDatas[1])       //tileDatas[0] = NotPlowalbe, tileDatas[1] = Plowalbe
+            if (mouseTileData == tileDatas[1])       //tileDatas[0] = NotPlowalbe, tileDatas[1] = Plowalbe
             {
                 isPlowable = true;
             }
