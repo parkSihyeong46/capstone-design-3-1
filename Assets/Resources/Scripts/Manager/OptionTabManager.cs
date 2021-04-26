84511d54331f508e2d7b24371599cdfc13cb585d
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-04-26 20:31:35 +0900

    옵션 UI 선택 구현
    1. 옵션 UI 선택
    - 옵션창에서 선택한 옵션임을 인지할 수 있도록 선택 시 버튼 위치를 밑으로 조금 이동되도록 구현,
    
    - 옵션창을 닫았다가 다시 켜도 이전에 눌렀던 옵션기록이 남아있도록 구현

---
+++ Assets/Resources/Scripts/Manager/OptionTabManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionTabManager : MonoBehaviour
+{
+    private OptionState optionState = OptionState.INVENTORY;
+    private OptionState prevOptionState = OptionState.INVENTORY;
+
+    private const float normalYPosYPos = 295;
+    private const float selectYPos = 281;
+
+    public List<Transform> button;
+    public List<GameObject> option;
+
+    private void OnEnable()
+    {
+        for(int i = 0; i < option.Count; i ++)
+        {
+            if (null == option[i])
+                continue;
+
+            option[i].SetActive(false);
+        }
+
+        button[(int)optionState].localPosition = new Vector3(
+            button[(int)optionState].localPosition.x, selectYPos, button[(int)optionState].localPosition.z);
+
+        if (option.Count >= (int)optionState)
+        {
+            if (null == button[(int)optionState])
+                return;
+
+            option[(int)optionState].SetActive(true);      
+        }
+    }
+
+    public void SetOptionState(int state)
+    {
+        if (0 > state || (int)OptionState.EXIT < state)
+            return;
+
+        prevOptionState = optionState;
+        optionState = (OptionState)state;
+
+        button[(int)prevOptionState].localPosition = new Vector3(
+            button[(int)prevOptionState].localPosition.x, normalYPosYPos, button[(int)prevOptionState].localPosition.z);
+
+        OnEnable();
+    }
+}
+
+[SerializeField]
+public enum OptionState
+{
+    INVENTORY = 0,
+    STAT = 1,
+    INTIMACY = 2,
+    MAP = 3,
+    CREATE = 4,
+    COLLECT = 5,
+    OPTION = 6,
+    EXIT = 7,
+}
