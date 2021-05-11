2c20bd95f8306655b48e405fcb2c2bceac96e02a
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-05-11 18:57:05 +0900

    채광 + 애니메이션 + (벌목,채광) 딜레이 애니메이션
    1. 광질 할 수 있게 돌맹이 구현, 기본세팅
    
    2. 채광 애니메이션 구현
    
    3. 벌목, 채광
    - 해당 애니메이션 재생 시 딜레이 시간을 가지고 애니메이션을 재생하도록 딜레이 있는 애니메이션 제작

---
+++ Assets/Scripts/Interact/Interact_CrushRock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_CrushRock : Interact
+{
+    public List<Item> spawnItemList = new List<Item>();
+    private Animation rockAnim;
+    public int interactCount = 0;
+    private int accumInteractCount = 0;
+
+    private void Start()
+    {
+        itemID = Item.ItemID.Pick;
+        spawnItemList.Add(ItemManager.Instance.GetItem((int)Item.ItemID.Rock).DeepCopy());
+        rockAnim = GetComponent<Animation>();
+    }
+
+    public override void DoInteract(Character character, Item.ItemID itemID)
+    {
+        Debug.Log("돌에 필요한 도구: " + this.itemID + "   들고있는 도구: " + itemID);
+
+        Player_Manager player_Manager = character.GetComponent<Player_Manager>();
+
+        if (itemID == this.itemID)
+        {
+            player_Manager.RunAnimation("Work");
+            accumInteractCount++;
+            rockAnim.Play();
+
+            if (accumInteractCount >= interactCount)
+            {
+                foreach (Item spawnItem in spawnItemList)
+                {
+                    // 이 오브젝트 위치를 기반으로 아이템 생성
+                    ItemSpawnManager.Instance.SpawnItem(transform.position, spawnItem);
+                }
+                // 도구에 맞았으면 이 오브젝트를 Destroy 한다
+                Destroy(gameObject);
+            }
+        }
+        else { return; }
+    }
+
+}
